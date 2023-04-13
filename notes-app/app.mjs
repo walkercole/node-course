3e4a247968df4bdb72eda3e494d306a7bff2e73b
walkercole <besidethebrave@gmail.com>
2023-04-13 15:27:51 -0700

    3 files modified

--- notes-app/app.mjs
+++ notes-app/app.mjs
@@ -4,9 +4,9 @@ import getNotes from './notes.js';
 import chalk from 'chalk';
 
 const note = getNotes();
-const greenMsg = chalk.bold.inverse.italic.green('Success!!');
+const greenMsg = chalk.bold.inverse.green('Success!!');
 console.log(chalk.green(note));
 console.log(greenMsg);
 
-console.log(isEmail('walker@gmail.com'));
-console.log(isURL('walkercole.github.io'));
+console.log(chalk.bold.inverse.blue(isEmail('walker@gmail.com')));
+console.log(chalk.bold.black.inverse(isURL('walkercole.github.io')));
