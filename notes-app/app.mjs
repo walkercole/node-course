9b7b6452b1578834fcf154c5dbea12bd400e066f
walkercole <besidethebrave@gmail.com>
2023-04-13 11:29:49 -0700

    2 files modified

--- notes-app/app.mjs
+++ notes-app/app.mjs
@@ -1,6 +1,11 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, appendFileSync } from 'fs';
 
 writeFileSync(
   'notes.txt',
   'My name is Walker and this text was updated from Node.JS'
+);
+
+appendFileSync(
+  'notes.txt',
+  ' ...and this text was appended via the appendFileSync module'
 );
