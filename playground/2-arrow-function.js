cc495a9d532629a30cbc837678fbe7c36a517850
walkercole <besidethebrave@gmail.com>
2023-04-17 12:47:56 -0700

    1 file modified

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -2,11 +2,27 @@
 //   return x * x;
 // };
 
+// arrow function
 // const square = (x) => {
 //   return x * x;
 // };
 
 // implicit return
-const square = (x) => x * x;
+// const square = (x) => x * x;
 
-console.log(square(6));
+// console.log(square(6));
+
+const event = {
+  name: 'Tech Meet up',
+  guestList: ['Walker', 'Jen', 'Mike'],
+  // es6 definition syntax
+  printGuestList() {
+    console.log('Guest List for ' + this.name);
+    // arrow functions inherit their `this` value, makes them poor candidates for methods
+    this.guestList.forEach((guest) => {
+      console.log(guest + ' is attending ' + this.name);
+    });
+  },
+};
+
+event.printGuestList();
