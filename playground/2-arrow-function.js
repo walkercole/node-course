30c3c9a92012d241bed11cb998373b0fba2433b0
walkercole <besidethebrave@gmail.com>
2023-04-17 12:38:02 -0700

    3 files added

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,12 @@
+// const square = function (x) {
+//   return x * x;
+// };
+
+// const square = (x) => {
+//   return x * x;
+// };
+
+// implicit return
+const square = (x) => x * x;
+
+console.log(square(6));
