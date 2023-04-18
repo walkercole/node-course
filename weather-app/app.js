cb64a53413449a010b3d9892128d44667ff7ae36
Walker Cole <besidethebrave@gmail.com>
2023-04-18 10:45:47 -0700

    move to local

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,6 @@ const url =
   'http://api.weatherstack.com/current?access_key=ad908a88613699a10e5c0c01d1b73279&query=New%20York';
 
 request({ url: url }, (error, response) => {
-  // const data = JSON.parse(response);
-  console.log(response);
+  const data = JSON.parse(response.body);
+  console.log(data.current);
 });
