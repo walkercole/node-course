9ba34a99742ae7238900ac05c66ccb336ca7e86c
walkercole <besidethebrave@gmail.com>
2023-04-18 09:39:42 -0700

    1 file added

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('Starting Application...');
+
+setTimeout(() => {
+  console.log('1 second timer.');
+}, 1000);
+
+setTimeout(() => {
+  console.log('timer instant');
+}, 0);
+
+console.log('Stopping Application...');
